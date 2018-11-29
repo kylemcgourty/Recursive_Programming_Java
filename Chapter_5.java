da2fb4e6dfbbecdde226c0fdf11fe7e9a295ffbb
Kyle McGourty <kyle.mcgourty@gmail.com>
2018-11-29 14:06:08 -0800

    finished Chapter 5

--- Chapter_5.java
+++ Chapter_5.java
@@ -3,18 +3,27 @@ import java.util.*;
 public class Chapter_5 {
 
     Chapter_5(){
-//       Exercise_5_1 answer = new Exercise_5_1(42281);
-//       Exercise_5_2 answer = new Exercise_5_2();
-//       int[] unsorted_list = new int[]{1,3,7,2,2,5,1,5,9};
-//       Exercise_5_3 answer = new Exercise_5_3(unsorted_list);
+       Exercise_5_1 answer = new Exercise_5_1(42281);
+       Exercise_5_2 answer = new Exercise_5_2();
+       int[] unsorted_list = new int[]{1,3,7,2,2,5,1,5,9};
+       Exercise_5_3 answer = new Exercise_5_3(unsorted_list);
 
-         int[] sorted_list = new int[]{1,2,2,3,5,6,6,7,8,9,9,10};
-//        Exercise_5_4 answer = new Exercise_5_4(sorted_list);
-//        System.out.println(answer.binary_search(5, 0,11));
+       int[] sorted_list = new int[]{1,2,2,3,5,6,6,7,8,9,9,10};
+       Exercise_5_4 answer = new Exercise_5_4(sorted_list);
+       System.out.println(answer.binary_search(5, 0,11));
 
-         Exercise_5_6 answer = new Exercise_5_6(sorted_list, 6);
+       Exercise_5_6 answer = new Exercise_5_6(sorted_list, 6);
 
+       int[] sorted_list_with_index_match = new int[]{-3, -2, -1, 3, 5, 9, 11};
+       Exercise_5_7 answer = new Exercise_5_7(sorted_list_with_index_match);
+       System.out.print("The answer " + answer.findIndexWithEqualValue(0, 6));
 
+       int[] listOfEvensAndOdds = new int[]{2,4,-2, 12,8,9, 7, 5, 11};
+       Exercise_5_8 answer = new Exercise_5_8(listOfEvensAndOdds);
+       System.out.print("The answer " + answer.determineGreatestEvenIndex(0, 8));
+
+       Exercise_5_9 answer = new Exercise_5_9(2, 2.5);
+       System.out.println(answer.determineApproximation(2, 9));
     }
 }
 
@@ -179,6 +188,88 @@ class Exercise_5_6 {
             return booleanBinarySearch(newArray, searchValue);
         }
 
+    }
+
+}
+
+
+class Exercise_5_7 {
+    int []integers;
+    Exercise_5_7(int[] integer_array){
+         this.integers= integer_array;
+    }
+
+    public int findIndexWithEqualValue( int lo, int high) {
+
+        if (lo == high) return -1;
+
+        int mid = lo + (high-lo)/2;
+
+        if (mid == this.integers[mid]) {
+            return mid;
+        }
+        if (mid < this.integers[mid]){
+            return findIndexWithEqualValue(lo, mid-1);
+        }
+        if (mid > this.integers[mid]){
+            return findIndexWithEqualValue(mid + 1, high);
+        }
+
+        return -1;
+    }
+}
+
+
+class Exercise_5_8 {
+    int[] integers;
+    Exercise_5_8(int[] list){
+        this.integers = list;
+    }
+
+    int determineGreatestEvenIndex(int lo, int high){
+
+        if (lo == high) return -1;
+
+        int mid = lo + (high-lo)/2;
+
+        if (this.integers[mid] % 2 == 0){
+
+            if (this.integers[mid + 1] % 2 ==1) {
+                return mid;
+            }
+            return determineGreatestEvenIndex(mid+1, high);
+        }
+        if (this.integers[mid] % 2 == 1){
+            return determineGreatestEvenIndex(lo, mid-1);
+        }
+
+        return -1;
+
+    }
+}
+
+
+class Exercise_5_9{
+    int soughtValue;
+    double approximation;
+    Exercise_5_9(int desiredValue, double approximation ){
+        this.soughtValue = desiredValue;
+        this.approximation = approximation;
+    }
+
+    double determineApproximation(int n, double value){
+        if (n == 0) return this.approximation;
+
+            return this.determineApproximation(n-1, value) - this.implementFunction(n, value)/ this.implementDerivative(n, value);
+
+    }
+
+    double implementFunction(int n, double value){
+        return Math.pow(this.determineApproximation(n-1, value), 2) - value;
+    }
+
+    double implementDerivative(int n, double value){
+        return 2*this.determineApproximation(n-1, value);
     }
 
 }
